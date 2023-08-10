11c87d4617896542f49a51e018832a123bfd2f3b
MHervian <mhervian0@gmail.com>
2023-08-11 05:36:06 +0700

    Change footer content

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,11 +1,50 @@
 <footer>
-    <div class="container">
-    <div class="row">
-        <div class="col-12 text-center">
-        <p class="pt-4 pb-2">
-            2020 Copyright Store. All Rights Reserved.
-        </p>
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-md-4 col-lg-4 col-12 mb-4">
+                <h4>Jahe Store &amp; Caffee</h4>
+                <p>Jahe Store &amp; Caffee, kafe dengan menu utama minuman tradisional yang kekinian dan Instagramable.</p>
+                <hr />
+                <h5>Temukan Kami Di</h5>
+                <a href=""><i class='fab fa-facebook-square' style='font-size:24px; color: gray;'></i></a>
+                <a href="" class="ml-2"><i class='fab fa-instagram' style='font-size:24px; color: gray;'></i></a>
+                <a href="" class="ml-2"><i class='fab fa-youtube' style='font-size:24px; color: gray;'></i></a>
+                <a href="" class="ml-2"><i class='fab fa-twitter' style='font-size:24px; color: gray;'></i></a>
+            </div>
+            <div class="col-6 col-md-2 mb-4">
+                <h5>Info JaheStore</h5>
+                <ul class="nav flex-columns">
+                    <li class="nav-item d-block"><a href="#" class="nav-link pl-0" style="color: gray;">Tentang Kami</a></li>
+                    <li class="nav-item d-block"><a href="#" class="nav-link pl-0" style="color: gray;">Testimoni</a></li>
+                    <li class="nav-item d-block"><a href="#" class="nav-link pl-0" style="color: gray;">Privasi</a></li>
+                    <li class="nav-item d-block"><a href="#" class="nav-link pl-0" style="color: gray;">Ketentuan</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h5>Bantuan</h5>
+                <ul class="nav flex-columns">
+                    <li class="nav-item d-block"><a href="#" class="nav-link pl-0" style="color: gray;">Kontak Kami</a></li>
+                    <li class="nav-item d-block"><a herf="#" class="nav-link pl-0" style="color: gray;">Berbelanja</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-lg-4 col-12">
+                <div class="mb-2">
+                    <address><i class="fa fa-map-marker" style="font-size:24px"></i> Jl Swakarsa 1b Rt 004 Rw 003 No.23 Pondok Kelapa Jakarta timur</address>
+                </div>
+                <div class="mb-2">
+                    <p><i class="fas fa-envelope"></i> Yogipamungkas62@gmail.com</p>
+                </div>
+                <div class="mb-2">
+                    <p><i class="fas fa-phone"></i> 08273823748</p>
+                </div>
+            </div>
         </div>
-    </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="pt-4 pb-2">
+                    2020 Copyright Store. All Rights Reserved.
+                </p>
+            </div>
+        </div>
     </div>
 </footer>
